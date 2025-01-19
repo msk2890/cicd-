5fddc1646afaf2edce75a3a9289eb0b8f1db03b0
MiguelAA <543435@STUDENT.FONTYS.NL>
2025-01-19 20:57:56 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello, World!'})
+
+
+@app.route('/goodbye', methods=['GET'])
+def goodbye():
+    return jsonify({'message': 'Goodbye, World!'})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
